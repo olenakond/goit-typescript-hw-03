fb999c7a15095cb8f8bf27ee0fdab46d68768c95
Olena <shapovalev@gmail.com>
2023-11-01 21:32:14 +0100

    hw

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,61 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random() * 100;
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: object;
+
+  constructor(key: object) {
+    this.key = key;
+  }
+
+  getKey(): object {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean;
+  protected key: object;
+  private tenants: object[];
+
+  constructor(key: object){
+    this.key = key;
+  }
+
+  public abstract openDoor(key: object): void;
+
+  public comeIn(person: object): void {
+    if (this.door === true) {
+      this.tenants.push(person);
+    }
+  }
+}
+
+class MyHouse extends House {
+  public openDoor(key: object): void {
+    if (this.key === key) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
-const house = new MyHouse(key);
 const person = new Person(key);
+
+const house = new MyHouse(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
